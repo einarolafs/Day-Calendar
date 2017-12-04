10ea3b3f4bae43af75a8c5531194b458b38b6cc0
Einar Ólafsson <einsiol@gmail.com>
2017-12-04 17:41:55 +0100

    Simplifying code and fixing position of time in the view

--- main.js
+++ main.js
@@ -1,34 +1,40 @@
-
 //Create the time of day on the side programmatically, so as to make the html document as clean as possible
 
-const calTime = ['9:00', '9:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '1:00', '1:30', '2:00', '2:30', '3:00', '3:30', '4:00', '4:30', '5:00', '5:30', '6:00', '6:30', '7:00', '7:30', '8:00', '8:30', '9:00']
+const time_of_day = ['9:00', '9:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '1:00', '1:30', '2:00', '2:30', '3:00', '3:30', '4:00', '4:30', '5:00', '5:30', '6:00', '6:30', '7:00', '7:30', '8:00', '8:30', '9:00']
 
+// Helper function to check if number is even or odd
 const is_even = (x) => !(x % 2);
 
+// Helper function to find the median of a row of numbers within an array
+const median = function find_median(values){
+	values.sort(function(a,b){
+  		return a-b;
+ 	});
+  	
+  	var half = Math.floor(values.length / 2);
+  
+  	if (values.length % 2)
+  		return values[half];
+  	else
+  		return (values[half - 1] + values[half]) / 2.0;
+}
+
+//Store reference to the DOM containers that will be populated with the time on the left side and the events
 const time_container = document.querySelector('.calendar .time');
 const events_container = document.querySelector('.calendar .events')
 
-
-for(let i = 0; i < calTime.length; i++) {
+//Populate the left side with the time of day
+time_of_day.forEach(function(time, i){
 	let container = document.createElement("div");
-	const twelve_hour = i >= 6 ? 'pm' : 'am';
-	let content = document.createTextNode(`${calTime[i]} ${is_even(i) ? twelve_hour : ''}`);
-	container.append(content); time_container.append(container);
-}
+	const twelve_hour = i >= 6 ? '<span>pm</span>' : '<span>am</span>';
+	let content = `${time_of_day[i]} ${is_even(i) ? twelve_hour : ''}`;
+	container.insertAdjacentHTML('beforeend', content); time_container.append(container);
+});
 
-function median(values){
-	values.sort(function(a,b){
-  	return a-b;
-  });
-  var half = Math.floor(values.length / 2);
-  
-  if (values.length % 2)
-  	return values[half];
-  else
-  	return (values[half - 1] + values[half]) / 2.0;
-}
 
 const layOutDay = function createEvents (events) { 
+
+	// Star with some error handling to make sure the array of events and the events them selfs are correct
 
 	var pleseProvide = 'Please provide an array of objects in the format of {start:50, end:100}'
 
@@ -36,10 +42,12 @@ const layOutDay = function createEvents (events) {
 		console.error('No arguments to process. ' + pleseProvide)
 		return
 	}
+
 	if (!(events instanceof Array)) {
 		console.error('The variable given is not an Array and therefor cannot be processed. ' + pleseProvide)
 		return
 	}
+
 	let errorInput = [];
 	events.forEach(function(event) {
 		if(event.start < 0 || event.end > 720) {
@@ -55,16 +63,18 @@ const layOutDay = function createEvents (events) {
 	});
 
 	if (!!errorInput.length) {return}
-	
 
-	//Clear container of previous content
-	while (events_container.hasChildNodes()) {
-	    events_container.removeChild(events_container.lastChild);
-	}
+	// End of error checking
 
+
+
+	//Clear events container of previous content
+	events_container.innerHTML = '';
+
+	//Create simple content for each event
 	const content = "<div><h4>Sample event</h4><p>Sample event</p></div>"
 
-	//Container to hold all events
+	//Object to hold all the events
 	events_spaces = {}
 
 	events.forEach( function(event, index) {
@@ -73,80 +83,68 @@ const layOutDay = function createEvents (events) {
 
 		container.insertAdjacentHTML('beforeend', content);
 
-		let width = 600, position = 0, style = container.style;
-
-		// Array to store all events that are overlapping with the event being created
-		let how_many_events = []
-
-		//count the amount of events that overlap with this one and store each one in an array
-		for (past in events_spaces) {
-
-			if (events_spaces[past].end > event.start && events_spaces[past].start < event.end) {
-
-				how_many_events.push(events_spaces[past])
-
+		let properties = {
+			width: 600,
+			position: 0,
+			get_position: function(){
+				return this.width * this.position;
 			}
 		}
 
-		//Divide with among all items that overlap
-		width = (width / (how_many_events.length + 1));
-		let radius = width / 2;
+		let overlap = {
+			events: [],
+			count: [1],
+			index: 1,
+		}
 
-		console.log(width, radius);
+		// Function to be used to style multiple overlapping events.
+		const style_multiple_events = function(event, index, count, width) {
+			position = ((index - median(count)) * width);
 
-		let count_colition = [1];
+			event.style.width = width + 'px';
+			event.style.left = 'calc(50% - ' + (width / 2) + 'px';
+			event.style.transform = 'translatex('+ position +'px)'
+		}
 
-		how_many_events.forEach( function(element, index) {
-			count_colition.push(index + 2);
-		});
+		//count the amount of events that overlap with this one and store each one in an array
+		for (past in events_spaces) {
+			if (events_spaces[past].end > event.start && events_spaces[past].start < event.end) {
+				overlap.events.push(events_spaces[past]);
+				overlap.count.push(overlap.count.length + 1);
+			}
+		}
 
-		//console.log('count_colition', count_colition)
+		//Divide width among all items that overlap
+		properties.width = (properties.width / (overlap.events.length + 1));
 
-		let overlapping_index = 1; 
-		//First if is when overlapping items are more then 1
-		if(how_many_events.length > 1) {
-			for (past in how_many_events) {
-				position = ((overlapping_index - median(count_colition)) * width)
-				how_many_events[past].container.style.width = width + 'px';
-				how_many_events[past].container.style.left = 'calc(50% - ' + radius + 'px';
-				how_many_events[past].container.style.transform = 'translatex('+ position +'px)'
-
-				overlapping_index++;
+		if(overlap.events.length > 1) {
+			for (past in overlap.events) {
+				style_multiple_events(overlap.events[past].container, overlap.index, overlap.count, properties.width);
+				overlap.index++;
 			} 
 		} else {
-			for (past in how_many_events) { 
-				if (how_many_events[past].position === width) {
-					position = 0;
+			for (past in overlap.events) { 
+				if (overlap.events[past].position === properties.width) {
+					properties.position = 0;
 				} else { 
-					how_many_events[past].container.style.width = width + 'px';
-					position++ 
+					overlap.events[past].container.style.width = properties.width + 'px';
+					properties.position++ 
 				}
 			}
 		}
 
-		style.width = width + 'px';
+		if (overlap.count.length > 2) style_multiple_events(container, overlap.index, overlap.count, properties.width);
+		else container.style.left = properties.get_position() + 'px';
 
-		//console.log(overlapping_index);
+		container.style.width = properties.width + 'px';
+		container.style.height = (event.end - event.start) + 'px';
+		container.style.top = event.start + 'px';
 
-		if (count_colition.length > 2) {
-		 position = ((overlapping_index - median(count_colition)) * width)
-		 style.left = position + 'px';
-		 style.left = 'calc(50% - ' + radius + 'px';
-		 style.transform = 'translatex('+ position +'px)'
-		 console.log('move')
-		}
-		else {
-			style.left = (width * position) + (position > 0 ? 10 : 0) + 'px';
-		}
-
-		style.height = (event.end - event.start) + 'px';
-		style.top = event.start + 'px';
-
-		// Add the item to an object that will store all the items to display, so that they can be retrieved and calculated against
+		// Add the event to an object that will store all the events to display, so that they can be retrieved and calculated against
 		events_spaces[Math.random().toString(36).substr(2, 10)] = {
 			end: event.end,
 			start: event.start,
-			position: (width * position),
+			position: properties.get_position(),
 			container: container,
 		};
 
@@ -155,8 +153,6 @@ const layOutDay = function createEvents (events) {
 	for (event in events_spaces) {
 		events_container.appendChild(events_spaces[event].container);
 	}
-
-
 }
 
 layOutDay([{start: 30, end: 150}, {start: 540, end: 600}, {start: 560, end: 620}, {start: 610, end: 670} ]);
